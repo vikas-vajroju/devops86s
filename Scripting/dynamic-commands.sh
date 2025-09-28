7457b8b01acced94e704b82cf667bc1cd0b3ac1b
vikas vajroju <vikas.devops86s@gmail.com>
2025-09-28 14:22:45 +0530

    added files

---
+++ Scripting/dynamic-commands.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+set -euo pipefail
+START_TIME=$(date +%s)
+
+
+sleep 2
+
+END_TIME=$(date +%s)
+
+TOTAL_TIME=$(($END_TIME-$START_TIME))
+
+echo "script executed in $TOTAL_TIME"
---
+++ Scripting/read_command.sh
@@ -0,0 +1,3 @@
+echo "enter you PIN"
+read -s PIN # -s for silent mode, input won't be displayed on the terminal
+echo "you entered: $PIN"
