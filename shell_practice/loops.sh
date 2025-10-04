2207354abbbff4e52447fd4e3a7bdc37186a0489
vikas vajroju <vikas.devops86s@gmail.com>
2025-10-05 00:06:46 +0530

    " "

--- shell_practice/loops.sh
+++ shell_practice/loops.sh
@@ -24,10 +24,10 @@ echo "Scripting starting $(date)"
 
 for package in $@
 do
-    dnf list installed $package &>>$log_file
+    dnf list installed "$package" &>>"$log_file"
     if [ $? is -ne 0 ]; then
-        dnf install $package -y &>>$log_file
-        validate $? $package
+        dnf install "$package" -y &>>"$log_file"
+        validate $? "$package"
     else
         echo "$package is already installed ....SKIPPING..."
     fi
