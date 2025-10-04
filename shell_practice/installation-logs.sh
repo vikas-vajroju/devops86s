f5da851e2d42dcef17e8d2fe501854b3b4076651
vikas vajroju <vikas.devops86s@gmail.com>
2025-10-04 22:44:16 +0530

    done

--- shell_practice/installation-logs.sh
+++ shell_practice/installation-logs.sh
@@ -1,5 +1,5 @@
-#!/bin/bash
-#installtion script
+# #!/bin/bash
+# #installtion script
 
 
 USERID=$(id -u)
@@ -8,44 +8,44 @@ script_name=$( echo $0 | cut -d "." -f1 )
 log_file="$log_folder/$script_name.log"
 mkdir -p $logs_folder
 
-if [ $USERID -ne 0 ]; then
-    echo "ERROR : please run the script with root user access "
-    exit 1
-fi
+# if [ $USERID -ne 0 ]; then
+#     echo "ERROR : please run the script with root user access "
+#     exit 1
+# fi
 
-validate(){
-    if [ $1 -ne 0 ]; then
-        echo "error :: installation $2 failed"
-        exit 1
-    else
-        echo "$2 Installation is success"
-    fi
-}
+# validate(){
+#     if [ $1 -ne 0 ]; then
+#         echo "error :: installation $2 failed"
+#         exit 1
+#     else
+#         echo "$2 Installation is success"
+#     fi
+# }
 
 dnf list installed mysql &>>$log_file
 
-if [ $? -ne 0 ]; then
-    dnf install mysql -y &>>$log_file
-    validate $? MYSQL
-else
-    echo "MYSQL skip Installation as package is already installed"
-fi
+# if [ $? -ne 0 ]; then
+#     dnf install mysql -y &>>$log_file
+#     validate $? MYSQL
+# else
+#     echo "MYSQL skip Installation as package is already installed"
+# fi
 
-dnf list installed nginx &>>$log_file
+# dnf list installed nginx &>>$log_file
 
-if [ $? -ne 0 ]; then
-    dnf install nginx -y &>>$log_file
-    validate $? NGINX
-else
-    echo "NGINX skip Installation as package is already installed"
-fi
+# if [ $? -ne 0 ]; then
+#     dnf install nginx -y &>>$log_file
+#     validate $? NGINX
+# else
+#     echo "NGINX skip Installation as package is already installed"
+# fi
 
 
-dnf list installed python3 &>>$log_file
+# dnf list installed python3 &>>$log_file
 
-if [ $? -ne 0 ]; then
-    dnf install python3 -y &>>$log_file
-    validate $? PYTHON3
-else
-    echo "Python3 skip Installation as package is already installed"
-fi
+# if [ $? -ne 0 ]; then
+#     dnf install python3 -y &>>$log_file
+#     validate $? PYTHON3
+# else
+#     echo "Python3 skip Installation as package is already installed"
+# fi
