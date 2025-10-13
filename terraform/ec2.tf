bac5c92d8754a8da77069befdfd322f48bc579bf
vikas vajroju <vikas.devops86s@gmail.com>
2025-10-13 20:25:26 +0530

    updated

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -1,6 +1,7 @@
-resource "aws_instance" "Terraform" {
-    ami           = "ami-09c813fb71547fc4f"
+resource "aws_instance" "example" {
+    ami           = ami-09c813fb71547fc4f
     instance_type = "t3.micro"
+
     tags = {
       Name = "terraform"
       Terraform = "true"
