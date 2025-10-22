5fd6fcc333f7f296666370f6eb3d43cebf939423
Vikas Vajroju <vikas.devops86s@gmail.com>
2025-10-22 10:58:12 +0530

    Delete ec2.tf

--- ec2.tf
+++
@@ -1,9 +0,0 @@
-resource "aws_instance" "example" {
-    ami           = ami-09c813fb71547fc4f
-    instance_type = "t3.micro"
-
-    tags = {
-      Name = "terraform"
-      Terraform = "true"
-  }
-}
